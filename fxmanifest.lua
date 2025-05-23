64fb641f82ddda1661d9f118ad2957e05fdf43d0
Josh.dev <Joshua-Rotherham@outlook.com>
2025-05-23 23:33:54 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'josh.dev'
+description 'Standalone /dv delete-vehicle command (Discord: https://discord.gg/NzNs3nsCA6)'
+version '1.0.0'
+
+client_scripts { 'client.lua' }
+server_scripts { 'server.lua' }
